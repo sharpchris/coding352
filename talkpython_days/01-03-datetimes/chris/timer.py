f5c1746d1143140b9b1ba084cb3fd5aa729712eb
Chris Sharp <sharpchris@gmail.com>
2018-11-29 13:54:46 -0500

    Created a timer that counts down the seconds from an inputted number of minutes

---
+++ talkpython_days/01-03-datetimes/chris/timer.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+from time import sleep
+
+print("Welcome to the Sharp Timer!")
+number_minutes = input("How many minutes do you want to put on the timer? ")
+
+# TODO: Logic to only accept <24 hours worth of seconds
+time_interval = timedelta(minutes=int(number_minutes))
+
+final_time = datetime.now() + time_interval
+print("The timer will go off at " + str(final_time))
+
+while datetime.today() < final_time:
+    print("There are " + str((final_time - datetime.now()).seconds) + " seconds left on the timer.")
+    sleep(1)
+
+print("RING RING RING! Time's up!!")
