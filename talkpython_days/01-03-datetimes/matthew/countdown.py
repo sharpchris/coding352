b706a0da9fd363a67122cde6ee731c62a30d6567
mmoga <matt.moga@gmail.com>
2018-12-06 14:20:46 -0500

    Finished birthday calculator

--- talkpython_days/01-03-datetimes/matthew/countdown.py
+++ talkpython_days/01-03-datetimes/matthew/countdown.py
@@ -13,18 +13,19 @@ dob_year = int(dob_data[2])
 dob = date(dob_year, dob_month, dob_day)
 
 #Calculate number of days lived
-# number_of_days = (today - dob).days
+number_of_days = (today - dob).days
+print("You\'ve lived for " + str(number_of_days) + " days. Yeesh!")
 
 #Calculate the number of days until next birthday
-# this_year = today.dob_year
+this_year = today.year
 
-# next_birthday = date(this_year, dob_month, dob_day)
-# if today < next_birthday:
-#     gap = (next_birthday - today).days
-#     print("Your birthday is in " + str(gap) + " days.")
-# elif today == next_birthday:
-#     print("Happy birthday, you!")
-# else:
-#     next_birthday = date(this_year + 1, dob_month, dob_day)
-#     gap = (next_birthday - today).days
-#     print("Your birthday is in " + str(gap) + " days.")
+next_birthday = date(this_year, dob_month, dob_day)
+if today < next_birthday:
+    gap = (next_birthday - today).days
+    print("Your birthday is in " + str(gap) + " days.")
+elif today == next_birthday:
+    print("Happy birthday, you!")
+else:
+    next_birthday = date(this_year + 1, dob_month, dob_day)
+    gap = (next_birthday - today).days
+    print("Your birthday is in " + str(gap) + " days.")
