69789f3c290ac96ac829b6b46a536ac7c672d866
Bryan Tobin <bryanfctobin@gmail.com>
2018-12-06 10:57:48 -0500

    Updating age game to Python 3.7

--- talkpython_days/01-03-datetimes/bryan/game.py
+++ talkpython_days/01-03-datetimes/bryan/game.py
@@ -9,10 +9,12 @@ print(todaydate)
 
 age = input("What is your age?")
 
+age = int(age)
+
 agedelta = (today.year - age)
 secondagedelta = (today.year - age - 1)
 
-birthdayoccur = raw_input("Has your birthday already passed this year?  Please respond with 'y' for yes, and 'n' for no.")
+birthdayoccur = input("Has your birthday already passed this year?  Please respond with 'y' for yes, and 'n' for no.")
 
 if birthdayoccur == 'y':
     print("You were born in " + str(agedelta))
