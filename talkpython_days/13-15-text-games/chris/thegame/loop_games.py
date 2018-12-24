bc84e000485e970f001ac72e96ddf11fa7fc2fb1
Chris Sharp <sharpchris@gmail.com>
2018-12-24 09:11:51 -0500

    Added mode

--- talkpython_days/13-15-text-games/chris/thegame/loop_games.py
+++ talkpython_days/13-15-text-games/chris/thegame/loop_games.py
@@ -1,5 +1,5 @@
 from main import play_game
-from statistics import mean, median
+from statistics import mean, median, mode
 
 NUMBER_OF_GAMES = 1000
 
@@ -11,4 +11,5 @@ outcomes.sort()
 print(f"The best game had {str(min(outcomes))} remaining.")
 print(f"The average game had {str(mean(outcomes))} cards remaining.")
 print(f"The median game had {str(median(outcomes))} cards remaining.")
+print(f"The mode was {str(mode(outcomes))} cards remaining.")
 print(outcomes)
